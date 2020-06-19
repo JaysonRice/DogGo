a8b65b99ad2c45d1973eafd3a1ce335f0997b09a
Jayson Rice <jayson.rice.95@gmail.com>
2020-06-19 10:56:16 -0400

    Made view model for walkers.

---
+++ Doggo/Models/View Models/WalkerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doggo.Models.View_Models
+{
+    public class WalkerViewModel
+    {
+    }
+}
---
+++ Doggo/Models/Walk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doggo.Models
+{
+    public class Walk
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Duration { get; set; }
+        public int WalkerId { get; set; }
+        public Walker Walker { get; set; }
+        public int DogId { get; set; }
+        public Dog Dog { get; set; }
+
+    }
+}
